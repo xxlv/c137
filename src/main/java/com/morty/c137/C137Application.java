be187de4e3ce2706f7d2f03d1a4e984aadac958f
Patrick95 <lawcy@qq.com>
2018-08-16 14:48:12 +0800

    init

---
+++ src/main/java/com/morty/c137/C137Application.java
@@ -0,0 +1,12 @@
+package com.morty.c137;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C137Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C137Application.class, args);
+	}
+}
---
+++ src/test/java/com/morty/c137/C137ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.morty.c137;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class C137ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
