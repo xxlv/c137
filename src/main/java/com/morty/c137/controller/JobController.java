6160a47ab1f34459440d257b8aa435e2fbcda0ff
Patrick95 <lawcy@qq.com>
2018-08-16 15:48:38 +0800

    test job controller

--- src/main/java/com/morty/c137/controller/JobController.java
+++ src/main/java/com/morty/c137/controller/JobController.java
@@ -1,4 +1,20 @@
 package com.morty.c137.controller;
 
+import com.morty.c137.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs")
 public class JobController {
+    @Autowired
+    private JobService jobService;
+
+    @GetMapping
+    public String index()
+    {
+        return "";
+    }
 }
--- src/main/java/com/morty/c137/service/JobService.java
+++ src/main/java/com/morty/c137/service/JobService.java
@@ -1,4 +1,8 @@
 package com.morty.c137.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JobService {
+
 }
