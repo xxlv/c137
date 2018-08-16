20f061727b0448a7b248e057830f78c4868ac5c0
ghost <lvxiang119@gmail.com>
2018-08-16 21:04:38 +0800

    Fix version

--- src/test/java/com/morty/c137/biz/impl/JobBizImplTest.java
+++ src/test/java/com/morty/c137/biz/impl/JobBizImplTest.java
@@ -1,15 +1,26 @@
 package com.morty.c137.biz.impl;
 
+import com.morty.c137.dao.JobMapper;
+import com.morty.c137.po.Job;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @ContextConfiguration(locations = {"classpath:spring-context.xml"})
 public class JobBizImplTest extends AbstractTestNGSpringContextTests {
 
+    @Resource
+    private JobMapper jobMapper;
+
     @Test
     public void testSaveJob() {
-        Assert.assertTrue(false);
+
+        List<Job> jobs=jobMapper.selectAll();
+        System.out.println(jobs.size());
+        Assert.assertTrue(true);
     }
 }
