362c152ce6295e3cc228c49ed60fc35254bc3ffe
ghost <lvxiang119@gmail.com>
2018-08-16 20:45:06 +0800

    Fix tests

--- src/test/java/com/morty/c137/biz/impl/JobBizImplTest.java
+++ src/test/java/com/morty/c137/biz/impl/JobBizImplTest.java
@@ -5,13 +5,11 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 @ContextConfiguration(locations = {"classpath:spring-context.xml"})
 public class JobBizImplTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testSaveJob() {
-        Assert.assertTrue(true);
+        Assert.assertTrue(false);
     }
 }
