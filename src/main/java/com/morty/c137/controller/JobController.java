c35c46dd7ef490788f0e526a4cf469b152a55d4d
ghost <lvxiang119@gmail.com>
2018-08-20 11:34:22 +0800

    Fix tests

--- src/main/java/com/morty/c137/controller/JobController.java
+++ src/main/java/com/morty/c137/controller/JobController.java
@@ -2,7 +2,6 @@ package com.morty.c137.controller;
 
 import com.morty.c137.biz.JobBiz;
 import com.morty.c137.po.Job;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
