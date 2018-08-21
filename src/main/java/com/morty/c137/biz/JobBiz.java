6dac38a6edf5c70c13fd011da4d85f8297560f55
Patrick95 <lawcy@qq.com>
2018-08-21 21:06:12 +0800

    add get job and update job routes

--- src/main/java/com/morty/c137/biz/JobBiz.java
+++ src/main/java/com/morty/c137/biz/JobBiz.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Service;
 
 public interface JobBiz {
 
+    public Job getJob(int id);
+
     public Boolean saveJob(Job job);
+
+    public Boolean updateJob(Job job);
 
     public Boolean deleteJob(int id);
 }
