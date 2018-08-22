efebbb83adcc81c76a0aa0e114c977027265e684
Patrick95 <lawcy@qq.com>
2018-08-22 20:01:00 +0800

    test schedule

--- src/main/java/com/morty/c137/C137Application.java
+++ src/main/java/com/morty/c137/C137Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableScheduling
 @ImportResource({"classpath:spring-context.xml"})
 public class C137Application {
 
